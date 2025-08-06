28f639e861483b2ef0c854bc76e4c5ce1843e41c
Eric <klingene@gmail.com>
2025-08-05 19:14:25 -0500

    Update scope data splitting

--- src/utils/scope.ts
+++ src/utils/scope.ts
@@ -1,25 +1,28 @@
 import { FitBitUserIDData } from "../types";
 
+export function splitData(data: string) {
+	return data.split(" ").map(s => s.trim()).filter(s => s.length > 0);
+}	
+
 export function validateScope(userData: FitBitUserIDData) {
 
-	const requiredScopes = (process.env.SCOPES_NEEDED || "").split(" ").map(s => s.trim()).filter(s => s.length > 0)
+	const requiredScopes = splitData(process.env.SCOPES_NEEDED ?? "")
 
-	const presentScopes = (userData?.scope ?? "").split(" ").map(s => s.trim()).filter(s => s.length > 0);
+	const presentScopes = splitData(userData?.scope ?? "")
 	const user_id = userData?.user_id ?? "user_id_not_found"
 	
 	const missingScopes = requiredScopes.filter(
       (scope: string) => !presentScopes.includes(scope)
     );
 
-	const availableScopes = requiredScopes.filter(
-      (scope: string) => presentScopes.includes(scope)
-    );
+	// const availableScopes = requiredScopes.filter(
+    //   (scope: string) => presentScopes.includes(scope)
+    // );
 
 	let allScopesPresent = missingScopes.length === 0
 	return {
 		user_id,
 		allScopesPresent,
-		availableScopes,
 		missingScopes,
 	}
 }
