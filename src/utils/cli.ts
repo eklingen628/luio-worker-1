7e2acc8af2f3996e7d8ab74376698d862a50849f
Eric <klingene@gmail.com>
2025-08-06 20:42:07 -0500

    Update cli to use two options with flags

--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -3,19 +3,22 @@ import { genDates } from './date';
 import { processUserData } from '../workflow/processors';
 import { getOneUserData, getAllUserData } from '../data/user';
 import { ConfigType, SCOPE_ACTIONS } from '../handlers/dataHandlers';
+import { sendEmail, dataDump } from './email';
 
-async function runOnDemand() {
-  const userId = process.argv[2];
-  const config = process.argv[3] as ConfigType | 'all';
-  const startDate = process.argv[4];
-  const endDate = process.argv[5] || undefined; 
+
+
+async function getDataOnDemand() {
+  const userId = process.argv[3];
+  const config = process.argv[4] as ConfigType | 'all';
+  const startDate = process.argv[5];
+  const endDate = process.argv[6] || undefined; 
   
   if (!userId || !startDate) {
-    console.log('Usage: npm run cli <userId> <config> <startDate> [endDate]');
-    console.log('Example: npm run cli 12345 getSleep 01-15-2025');
-    console.log('Example: npm run cli 12345 getSleep 01-15-2025 01-20-2025');
-    console.log('Example: npm run cli all all 01-15-2025 01-20-2025');
-    console.log('Valid Configs: ', SCOPE_ACTIONS.join(', '));
+    console.log('Usage: npm run cli get <userId> <config> <startDate> [endDate]');
+    console.log('Example: npm run cli get 12345 getSleep 01-15-2025');
+    console.log('Example: npm run cli get 12345 getSleep 01-15-2025 01-20-2025');
+    console.log('Example: npm run cli get all all 01-15-2025 01-20-2025');
+    console.log('Valid Configs: ', ["all", ...SCOPE_ACTIONS].join(', '));
     process.exit(1);
   }
 
@@ -61,8 +64,44 @@ async function runOnDemand() {
     console.error('Error:', error);
     process.exit(1);
   }
-  
-  process.exit(0);
+    process.exit(0);
 }
+
+
+async function runOnDemand() {
+
+  // console.log("Argv0: ",process.argv[0])
+  // console.log("Argv1: ",process.argv[1])
+  // console.log("Argv2: ",process.argv[2])
+  // console.log("Argv3: ",process.argv[3])
+
+  
+
+  const flag = process.argv[2];
+
+  
+  if (!flag) {
+    console.log('Invalid. You must provide a flag.');
+    console.log('Valid flags: email or get');
+    process.exit(1);
+  }
+  
+  if (flag === 'email') {
+    await sendEmail(dataDump);
+  } else if (flag === 'get') {
+    await getDataOnDemand();
+  } else {
+    console.log('Invalid flag.');
+    console.log('Valid flags: email or get');
+    process.exit(1);
+  }
+}
+
+
+
+
+
+
+
 
 runOnDemand();
