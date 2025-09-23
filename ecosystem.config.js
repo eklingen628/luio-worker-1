3b4e1fe46ffb6100675708501d3aa823244fc1d8
Eric <klingene@gmail.com>
2025-09-23 18:32:56 -0500

    Change pm2 error out

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      out_file: "/home/eric/.pm2/logs/combined-err-out.log",
+      error_file: "/home/eric/.pm2/logs/combined-err-out.log",
       env: {
         NODE_ENV: 'production'
       }
